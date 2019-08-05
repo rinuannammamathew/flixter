838697dd5d9aab6b96247fb8fbf9cca9a24d955b
Rinu Mathew <rinuannammamathew@gmail.com>
2019-08-05 06:43:58 +0000

    Add a good message describing what you actually changed since your last commit in here

--- Gemfile
+++ Gemfile
@@ -85,3 +85,9 @@ gem "figaro"
 
 
 gem "fog-aws"
+
+
+
+gem "mini_magick"
+
+gem 'carrierwave-aws'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_055905) do
+ActiveRecord::Schema.define(version: 2019_08_05_061627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_07_31_055905) do
     t.integer "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
