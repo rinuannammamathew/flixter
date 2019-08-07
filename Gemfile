e00ceaa8341fddbfc9e39fe49710ad7e97b0887d
Rinu Mathew <rinuannammamathew@gmail.com>
2019-08-07 07:11:49 +0000

    Add a good message describing what you actually changed since your last commit in here

--- Gemfile
+++ Gemfile
@@ -91,3 +91,6 @@ gem "fog-aws"
 gem "mini_magick"
 
 gem 'carrierwave-aws'
+
+
+gem 'stripe'
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,26 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def show
 
   end
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
+
+  def require_authorized_for_current_course
+    if current_lesson.section.course != current_user
+      redirect_to course_path, alert: 'Error Message Here'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
